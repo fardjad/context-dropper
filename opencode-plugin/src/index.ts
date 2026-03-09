a1a1fd9a22a42bd3ec0c1c79b07811fce9f1273e
Fardjad Davari <public@fardjad.com>
2026-03-09 04:56:46 +0100

    0.2.0

--- opencode-plugin/src/index.ts
+++ opencode-plugin/src/index.ts
@@ -1,8 +1,4 @@
-import {
-  type Hooks,
-  type Plugin,
-  tool,
-} from "@opencode-ai/plugin";
+import { type Hooks, type Plugin, tool } from "@opencode-ai/plugin";
 import { getPackageVersion } from "../../src/version/version";
 import { type Logger, createLogger } from "./logger";
 import { MessageHandler } from "./message-handler";
